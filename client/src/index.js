aeb553dd0f50a205ecb94389f84d61df5f05ccaa
pallavi1812 <upallavi1812@gmail.com>
2021-08-14 11:28:29 +0530

    added redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
-
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,7 @@
+
+
+const authReducer = () => {
+
+}
+
+export default authReducer;
