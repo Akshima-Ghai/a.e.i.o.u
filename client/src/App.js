c4858bd978b95279b776e744db396c0147212a80
pallavi1812 <upallavi1812@gmail.com>
2021-08-14 11:49:19 +0530

    added authorization

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import FrontPage from './components/FrontPage/FrontPage';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Navbar />
             <Switch>
                 <Route path="/" component={FrontPage} exact />
+                <Route path="/auth" component={Auth} exact />
             </Switch>
         </BrowserRouter>
     )
