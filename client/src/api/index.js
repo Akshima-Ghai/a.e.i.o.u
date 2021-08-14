3f3643af7d3514a1c9b99e3cf37ccc53a4f03be5
pallavi1812 <upallavi1812@gmail.com>
2021-08-14 11:41:50 +0530

    connected backend route

---
+++ client/src/api/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "http://localhost:5000" });
+//below function happens on every request
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem("profile")) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
+    }
+    return req;
+});
+
+export const signin = (formData) => API.post("/user/signin", formData);
+export const signup = (formData) => API.post("/user/signup", formData);
