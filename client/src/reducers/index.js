4ad6090b29025850c20808fb6d67cbe22001864c
SV-0 <sakshiverma8100@gmail.com>
2021-08-14 13:21:52 +0530

    added event reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
+import { combineReducers } from "redux";
+import auth from "./auth";
+import events from "./events";
 
-export default combineReducers({ auth });
+export default combineReducers({ auth, events });
