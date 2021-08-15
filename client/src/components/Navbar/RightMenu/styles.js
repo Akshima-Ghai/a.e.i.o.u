44f5f4682c884b10c53b340827501d07b82fadf4
SV-0 <sakshiverma8100@gmail.com>
2021-08-15 12:24:50 +0530

    changed styling

--- client/src/components/Navbar/RightMenu/styles.js
+++ client/src/components/Navbar/RightMenu/styles.js
@@ -1,41 +1,41 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    root: {
-        display: "flex",
+  root: {
+    display: "flex",
+  },
+  signInBtn: {
+    backgroundColor: "#068fa0",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#468fa0",
     },
-    signInBtn: {
-        backgroundColor: "#068fa0",
-        color: "#fff",
-        "&:hover": {
-            backgroundColor: "#cc5050",
-        },
-        "&:focus": {
-            backgroundColor: "#cc5050",
-        },
-        [theme.breakpoints.down("sm")]: {
-            padding: "0.5rem 0.2rem"
-        },
+    "&:focus": {
+      backgroundColor: "#468fa0",
     },
-    paper: {
-        marginRight: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      padding: "0.5rem 0.2rem",
     },
-    profile: {
-        color: theme.palette.getContrastText("#068fa0"),
-        backgroundColor: "#068fa0",
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+  profile: {
+    color: theme.palette.getContrastText("#068fa0"),
+    backgroundColor: "#068fa0",
+  },
+  icon: {
+    padding: "0",
+  },
+  button: {
+    "&:hover": {
+      backgroundColor: "#ececec",
     },
-    icon: {
-        padding: "0",
+    "&:focus": {
+      backgroundColor: "#ececec",
     },
-    button: {
-        "&:hover": {
-            backgroundColor: "#ececec",
-        },
-        "&:focus": {
-            backgroundColor: "#ececec",
-        },
-        "&:active": {
-            backgroundColor: "#ececec",
-        }
+    "&:active": {
+      backgroundColor: "#ececec",
     },
+  },
 }));
