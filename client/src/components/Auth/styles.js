6018145b50664fcf7e835778f1052236db53e253
SV-0 <sakshiverma8100@gmail.com>
2021-08-15 12:25:59 +0530

    changed auth form

--- client/src/components/Auth/styles.js
+++ client/src/components/Auth/styles.js
@@ -1,30 +1,46 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: theme.spacing(2),
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: theme.spacing(2),
+  },
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
     },
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-        },
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: "#068fa0",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    backgroundColor: "#068fa0",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#468fa0",
     },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
+    "&:focus": {
+      backgroundColor: "#468fa0",
     },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
+    margin: theme.spacing(3, 0, 2),
+  },
+  googleButton: {
+    backgroundColor: "#068fa0",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#468fa0",
     },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
+    "&:focus": {
+      backgroundColor: "#468fa0",
     },
-    googleButton: {
-        marginBottom: theme.spacing(2),
-    },
+    marginBottom: theme.spacing(2),
+  },
 }));
