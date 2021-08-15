f21ccc179df46194581747268b28f883455ba071
SV-0 <sakshiverma8100@gmail.com>
2021-08-15 12:26:30 +0530

    changed styling

--- client/src/components/EventsTable/styles.js
+++ client/src/components/EventsTable/styles.js
@@ -1,11 +1,12 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    title: {
-        textAlign: 'left',
-        marginLeft: '1rem'
-    },
-    table: {
-        marginTop: '2rem'
-    }
-}))
+  title: {
+    textAlign: "left",
+    marginLeft: "1rem",
+  },
+  table: {
+    borderRadius: "5px",
+    marginTop: "2rem",
+  },
+}));
